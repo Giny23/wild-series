ecf48ee6fdea9226be9244c296fe7bdcd079e8fb
Giny23 <vduquenoy23@gmail.com>
2023-05-22 16:54:32 +0200

    quest 6

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,14 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
         ]);
     }
+    #[Route('/{id<\d+>}', methods: ['GET'], name: 'show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id
+        ]);
+    }
+
+    /* a configurer dans les prochaines quêtes
+    #[Route('/new', methods: ['GET', 'POST'], name: 'new')]
+    public function new(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+        ]);
+    }
+    #[Route('/{id<\d+>}', methods: ['DELETE'], name: 'delete')]
+    public function new(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+        ]);
+    } */
 }
